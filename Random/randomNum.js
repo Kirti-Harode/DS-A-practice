a538224a27ac2d69c51bb3c7ac2d80e414abf24e
Kirti Harode <kikihrd20@gmail.com>
2022-03-15 19:56:07 -0700

    new

---
+++ Random/randomNum.js
@@ -0,0 +1,8 @@
+// You are given :
+
+// 1. a function that generates a random number between 0 and 1 (e.g. Math.random())
+// 2. an inscribed circle with a radius of 1
+// 3. the distance between two points can be calculated with: distance = sqrt( x^2 + y^2 )
+
+// Write a function estimatePi(n) that uses randomly generated points to estimate pi where n is the number of points plotted. 
+// A higher n value should yield a more accurate estimate.
