258daf721401e3f13672233f550a805dfb9e6b2a
Kirti Harode <kikihrd20@gmail.com>
2022-04-01 14:54:01 -0700

    structy

--- Structy/linkedLists/zipperLists.js
+++ Structy/linkedLists/zipperLists.js
@@ -90,4 +90,61 @@
 // zipperLists(one, w);
 // // 1 -> w -> 2 -> 3
 
+// iterative:
 
+const zipperLists = (head1, head2) => {
+    // var current1 equal to head 1
+  //   var current2 to equal head2
+  //   while current is not null
+  //   current1.next set equal to current2
+  //   current1 equal to current1.next
+  //   current2.next set equal to current1
+    
+  //   n = length of list 1
+  //   m = length of list 2 
+  //   time: O(min(n, m))
+  //   space: O(1)
+    let tail = head1;
+    let curr1 = head1.next;
+    let curr2 = head2;
+    let count = 0;
+    
+    while(curr1 !== null && curr2 !== null){
+      if(count % 2 === 0) {
+        tail.next = curr2;
+        curr2 = curr2.next;
+      }else{
+          tail.next = curr1;
+         curr1 = curr1.next;
+      }
+     
+        tail = tail.next;
+        count++;
+      
+    }
+    if(curr1 !== null) tail.next = curr1;
+    if(curr2 !== null) tail.next = curr2;
+    return head1;
+  };
+  
+
+
+//   recursive: 
+
+
+const zipperLists = (head1, head2) => {
+ 
+    // n = length of list 1
+    // m = length of list 2
+    // Time: O(min(n, m))
+    // Space: O(min(n, m))
+      if(head1 === null && head2 === null)  return null;
+      if(head1 === null) return head2;
+      if(head2 === null)  return head1;
+    
+      let next1 = head1.next;
+      let next2 = head2.next;
+      head1.next = head2;
+      head2.next = zipperLists(next1,next2);
+      return head1;
+};
