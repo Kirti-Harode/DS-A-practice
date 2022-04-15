f8ddb6563609ab5dbd81dca476f90c1bb5776c2f
Kirti Harode <kikihrd20@gmail.com>
2022-04-15 14:23:46 -0700

    binary substring

---
+++ leetCode/amazon/countBinarySubString.js
@@ -0,0 +1,56 @@
+var countBinarySubstrings = function(s) {
+    //     create an output count set to 0
+    //     loop over s
+    //     set start at index 0, increment i
+    //     count the first num freq, when that breaks save that index to an mid var
+    //     from mid find same num of other ele
+    //     if not equal to first nums freq then use the second freq and add it to output 
+    //     set start to mid now, and repeat
+    //     return outpu at the end
+        
+    //     let output = 0;
+    //     let i = 0;
+    //     let start = 0;
+    //     let count = 0;
+    //     while(i < s.length){
+    //         if(s[start] === s[i]){
+    //             i++;
+    //             count ++;   
+    //         }else{
+    //             start = i;
+    //             count = 0;
+    //         }
+            
+    //     }
+        
+        
+        
+    //     from solution
+        
+        let groups = [];
+        
+        let count = 0;
+        let start = 0;
+        let i = 0;
+        while(i < s.length){
+            if(s[start] === s[i]){
+                i++;
+                count ++;   
+            }else{
+                groups.push(count);
+                start = i;
+                count = 0;
+            }        
+        }
+        groups.push(count);
+        let total = 0;
+        for(let j = 0; j < groups.length-1; j++){
+            if(groups[j] <= groups[j+1]){
+                total += groups[j];
+            }else{
+                total += groups[j+1];  
+            }
+            
+        }
+        return total;
+};
