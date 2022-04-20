63f8d2eca17c0bd8aa49fe60245877a13034eb8c
Kirti Harode <kikihrd20@gmail.com>
2022-04-20 13:11:05 -0400

    connected comp

--- Structy/graphs/connectedComponentsCount.js
+++ Structy/graphs/connectedComponentsCount.js
@@ -45,3 +45,66 @@
 //   8: []
 // }); // -> 5
 
+
+
+const connectedComponentsCount = (graph) => {
+    //   create a count var to keep no of connected components
+    //   take all the keys from the adjacency and loop over them
+    //   create a set to keep track of visited nodes to avoid infinite loop
+    //   create a stack to keep track of nodes
+    //   while stack is not empty take current and iterate over its neighbors and push them to stack
+    //   return count at the end
+      
+    let count = 0;
+    let nodes = Object.keys(graph);
+    let visited = new Set();
+    
+    for(let node of nodes){
+    let stack = [node];
+    while(stack.length > 0){
+        let current = stack.pop();
+        if(visited.has(String(current))){
+        break;
+        }else{
+        visited.add(String(current));
+        }
+        for(let neighbor of graph[current]){
+        stack.push(neighbor);
+        }
+    }
+    // if(stack.length === 0){
+        count ++;
+    // } 
+    }
+    return count;
+};
+
+
+
+
+const connectedComponentsCount = (graph) => {
+    let visited = new Set();
+    let count = 0;
+    
+    for(let node in graph){
+      if(explore(graph, node, visited)){
+        count ++;
+      }
+    }
+    return count;
+  };
+  
+  function explore(graph, current, visited){
+    if(visited.has(String(current))) return false; //converting to String  bcos keys in hash are string so when we put that in set it still will be a sgtring so check every thing with string and add every node after coverting to string.
+    
+    visited.add(String(current));
+    
+    for(let neighbor of graph[current]){
+      explore(graph, neighbor, visited);
+    }
+    return true;
+  }
+//   n = number of nodes
+//   e = number edges
+//   Time: O(e)
+//   Space: O(n)  
