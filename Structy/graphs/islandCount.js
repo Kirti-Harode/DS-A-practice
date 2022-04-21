5a775631a4c987ea5043e303f0285bdd90728af2
Kirti Harode <kikihrd20@gmail.com>
2022-04-21 10:22:40 -0400

    island questions

---
+++ Structy/graphs/islandCount.js
@@ -0,0 +1,95 @@
+// island count
+// Write a function, islandCount, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+
+// test_00:
+// const grid = [
+//   ['W', 'L', 'W', 'W', 'W'],
+//   ['W', 'L', 'W', 'W', 'W'],
+//   ['W', 'W', 'W', 'L', 'W'],
+//   ['W', 'W', 'L', 'L', 'W'],
+//   ['L', 'W', 'W', 'L', 'L'],
+//   ['L', 'L', 'W', 'W', 'W'],
+// ];
+
+// islandCount(grid); // -> 3
+// test_01:
+// const grid = [
+//   ['L', 'W', 'W', 'L', 'W'],
+//   ['L', 'W', 'W', 'L', 'L'],
+//   ['W', 'L', 'W', 'L', 'W'],
+//   ['W', 'W', 'W', 'W', 'W'],
+//   ['W', 'W', 'L', 'L', 'L'],
+// ];
+
+// islandCount(grid); // -> 4
+// test_02:
+// const grid = [
+//   ['L', 'L', 'L'],
+//   ['L', 'L', 'L'],
+//   ['L', 'L', 'L'],
+// ];
+
+// islandCount(grid); // -> 1
+// test_03:
+//  const grid = [
+//   ['W', 'W'],
+//   ['W', 'W'],
+//   ['W', 'W'],
+// ];
+
+// islandCount(grid); // -> 0
+
+
+
+
+const islandCount = (grid) => {
+//   use nested loops to get r and c
+//   create a new function  explore which returns true if found l and false if found w;
+//   create visited set to keep track of visited nodes or pos
+//   increment count every time explore function return true;
+//   check for bounds of r and c
+//   in explore function base case are r and c bounds and if r and c is w return false
+//   check if position is visited or not
+//   add pos to set by converting them to string bcos in set cann't put a subarr because it checks by refence not val
+//   recursively call explor function for left right and top bottom pos
+//   return count at the end
+    
+    
+    const visited = new Set();
+    let count = 0;
+    
+    for(let r = 0; r < grid.length; r++){
+    for(let c= 0; c < grid[0].length; c++){
+        if(explore(grid, r, c, visited)){
+        count ++;
+        }
+    }
+    }
+    return count;
+};
+
+    
+const explore = (grid, r, c, visited) => {
+    const rowInBounds = 0 <= r && r < grid.length;
+    const colInBounds = 0 <= c && c < grid[0].length;
+    if(!rowInBounds || !colInBounds)  return false;
+    if(grid[r][c] === 'W') return false;
+    
+    const pos = r + ',' + c;
+    
+    if(visited.has(pos)) return false;
+    visited.add(pos);
+    
+    explore(grid, r - 1, c, visited);
+    explore(grid, r + 1, c, visited);
+    explore(grid, r, c - 1, visited);
+    explore(grid, r, c + 1, visited);
+    
+    return true;
+    
+}
+
+// r = number of rows
+// c = number of columns
+// Time: O(rc)
+// Space: O(rc)
---
+++ Structy/graphs/shortestPath.js
@@ -0,0 +1,146 @@
+// shortest path
+// Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+
+// test_00:
+// const edges = [
+//   ['w', 'x'],
+//   ['x', 'y'],
+//   ['z', 'y'],
+//   ['z', 'v'],
+//   ['w', 'v']
+// ];
+
+// shortestPath(edges, 'w', 'z'); // -> 2
+// test_01:
+// const edges = [
+//   ['w', 'x'],
+//   ['x', 'y'],
+//   ['z', 'y'],
+//   ['z', 'v'],
+//   ['w', 'v']
+// ];
+
+// shortestPath(edges, 'y', 'x'); // -> 1
+// test_02:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+
+// shortestPath(edges, 'a', 'e'); // -> 3
+// test_03:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+
+// shortestPath(edges, 'e', 'c'); // -> 2
+// test_04:
+// const edges = [
+//   ['a', 'c'],
+//   ['a', 'b'],
+//   ['c', 'b'],
+//   ['c', 'd'],
+//   ['b', 'd'],
+//   ['e', 'd'],
+//   ['g', 'f']
+// ];
+
+// shortestPath(edges, 'b', 'g'); // -> -1
+// test_05:
+// const edges = [
+//   ['c', 'n'],
+//   ['c', 'e'],
+//   ['c', 's'],
+//   ['c', 'w'],
+//   ['w', 'e'],
+// ];
+
+// shortestPath(edges, 'w', 'e'); // -> 1
+// test_06:
+// const edges = [
+//   ['c', 'n'],
+//   ['c', 'e'],
+//   ['c', 's'],
+//   ['c', 'w'],
+//   ['w', 'e'],
+// ];
+
+// shortestPath(edges, 'n', 'e'); // -> 2
+// test_07:
+// const edges = [
+//   ['m', 'n'],
+//   ['n', 'o'],
+//   ['o', 'p'],
+//   ['p', 'q'],
+//   ['t', 'o'],
+//   ['r', 'q'],
+//   ['r', 's']
+// ];
+
+// shortestPath(edges, 'm', 's'); // -> 6
+
+const shortestPath = (edges, nodeA, nodeB) => {
+    //   breadth first will be more useful here
+    //   convert edges in a graph
+    //   make a queue to track all nodes and level of the nodes, nodeA will be at level 0
+    //   create a visited set to keep track of visited nodes, to avoid going in loop and count one node twice
+    //   while queue is not empty
+    //   pop a node check if that is in visited
+    //   check if this node is nodeB or not, if true then return the level corresponding to that node from the queue
+    //   add all neighbors of current node with one level up in queue.
+      let visited = new Set([nodeA]);
+      let graph = convertToGraph(edges);
+      console.log(graph)
+      let queue = [{node: nodeA, level: 0}];
+      while(queue.length > 0){
+        let current = queue.shift();
+        
+        if(current.node === nodeB){
+          return current.level;
+        }
+        for(let neighbor of graph[current.node]){
+          if(!visited.has(neighbor)){
+            visited.add(neighbor);
+            queue.push({node: neighbor, level: current.level + 1}); 
+          }
+        }
+      }
+      return -1;
+    }
+function convertToGraph(edges){
+      let graph = {};
+      for(let edge of edges){
+        const [a, b] = edge;
+        if (!graph[a]) graph[a] = [];
+        graph[a].push(b)
+      
+        if (!graph[b]) graph[b] = [];
+        graph[b].push(a)
+            
+      }
+      return graph;
+};
+    
+    
+    
+const edges = [
+      ['w', 'x'],
+      ['x', 'y'],
+      ['z', 'y'],
+      ['z', 'v'],
+      ['w', 'v']
+    ];
+    
+shortestPath(edges, 'w', 'z'); // -> 2
+    
