c8da45aeea3760e9e76b70cda8b006cc4560b308
Kirti Harode <kikihrd20@gmail.com>
2022-06-26 14:51:17 -0700

    group anagram

--- leetCode/75Questions/groupAnagrams.js
+++ leetCode/75Questions/groupAnagrams.js
@@ -25,3 +25,51 @@
 // strs[i] consists of lowercase English letters.
 
 
+var groupAnagrams = function(strs) {
+    let letterNums = {'a': 2, 
+                      'b': 3, 
+                      'c': 5, 
+                      'd': 7, 
+                      'e': 11, 
+                      'f': 13, 
+                      'g':17, 
+                      'h':19, 
+                      'i':23, 
+                      'j':29,
+                      'k': 31,
+                      'l': 37,
+                      'm': 41,
+                      'n': 43,
+                      'o': 47,
+                      'p': 53,
+                      'q': 59,
+                      'r': 61,
+                      's': 67, 
+                      't': 71,
+                      'u': 73,
+                      'v': 79,
+                      'w': 83,
+                      'x': 89,
+                      'y': 97,
+                      'z': 101
+                     };
+    
+    let subList = {};
+    
+    for(let str of strs){
+        let pro = 1;
+        for(let char of str){
+            pro *= letterNums[char];
+        }
+        
+        if(subList[pro]){
+           subList[pro].push(str);
+        }else{
+           subList[pro] = [str];
+        }
+    }
+    
+    // console.log(subList);
+    
+    return Object.values(subList);
+};
