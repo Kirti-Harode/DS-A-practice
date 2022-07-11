c152890e29e0e6a6b3c7c146ec2695d17d58a26d
Kirti Harode <kikihrd20@gmail.com>
2022-07-10 17:14:51 -0700

    tree

--- Interviews/usingTree.js
+++ Interviews/usingTree.js
@@ -1,8 +1,8 @@
 class TreeNode {
-    constructor(){
+    constructor(val){
         this.val = val;
         this.children = [];
-        this.end = false;
+        // this.end = false;
     }
 }
 
@@ -11,12 +11,21 @@ class MyTree {
         this.root = new TreeNode("Data");
     }
 
-    insert(ele){
-
+    insert(node, parentNode){
+        // if(this.search(node, parentNode) === false){
+            parentNode.children.push(node);
+            // return true;
+        // }else {
+            // return false;
+        // }
     }
 
-    search(ele, currentNode){
-        
+    search(node, parentNode){
+        if(parentNode.children.includes(node)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
@@ -33,15 +42,31 @@ let a = [
 ]
 
 function parseData(input){
-    let root = new Node("data");
-
+    // let tree = new MyTree();
+    let root = new TreeNode("Data");
     for(let ele of input){
         let firstEle = ele[0];
         let secEle = ele[1];
         let thirdEle = ele[2];
 
-        if(!(root.children.includes(firstEle))){
-            root.children.push(new Node(firstEle));
+        
+        let outerNode = search(firstEle, root);
+        let innerNode = search(secEle, outerNode);
+        innerNode.children.push(new TreeNode(thirdEle));
+    }
+    return root.children;
+    // root.children.forEach(node => {node.children.forEach(innerNode => {console.log(innerNode)})});
+}
+
+function search(val, parentNode){
+    for(let node of parentNode.children){
+        if(node.val === val){
+            return node;
         }
     }
+    let node = new TreeNode(val);
+    parentNode.children.push(node);
+    return node;
 }
+
+console.log(parseData(a));
