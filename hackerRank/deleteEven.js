feace49ecce35ec947b34c828946f403883e5aeb
Kirti Harode <kikihrd20@gmail.com>
2022-07-18 14:25:55 -0700

    hacker rank challenge questions

---
+++ hackerRank/deleteEven.js
@@ -0,0 +1,9 @@
+// Given a linked list of integers written a link list containing only notes having odd integers in their node -> data field
+// Example :
+// n = 3
+// listHead = 1->4->7
+// the link a list containing only nodes having odd integers is 1->7
+
+// functional description: 
+// complete the function delete even in the editor below delete even has the following parameters:
+// List list head or reference to the head of the bottling list and returns Lester reference to the heart of the resulting linguist
---
+++ hackerRank/racingCar.js
@@ -0,0 +1,17 @@
+//  Chris is playing the racing car arcade game: in this game, Chris is controlling a car that can move sideways but the car keeps moving forward at all times. Chris can move the car into any lane at any moment, when changing lanes, the race car can move through an obstacle.
+// 
+
+// It can move to a line that is occupied. For example if the race cars in line 1 and line 2 is occupied he can move laundry but not to rain there are some obstacles on the track.
+// there are three lanes in the game and Chris starts the game from the middle lane. Deter mine the minimum sideways moment needed in order to complete the game. Note that a  movement at one moment be it from lane 1 to lane 2 or from lane 1 to lane 3 it counted as a single moment.
+
+
+// STDIN    Function
+// -----    --------
+//     →   obstacleLanes[] size n = 3
+//    →   obstacleLanes = [2, 1, 2]
+// output: 1
+// Chris can move the car to third Lane in the first second and will not need to move the car I can refer to the image of the problem statement.
+
+
+
+
