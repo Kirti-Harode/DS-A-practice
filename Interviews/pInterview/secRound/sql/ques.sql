dbb06a106ad1ddcbb2e05c993252c99a10edca19
Kirti Harode <kikihrd20@gmail.com>
2022-07-22 18:05:04 -0700

    sql part

---
+++ Interviews/pInterview/secRound/sql/ques.sql
@@ -0,0 +1,12 @@
+Like how movies relates to actors, directions, etc.
+
+I think my was blog posts and users, comments tables.
+each blog could have tags on what category it’s about
+1) One was like select all the blogs posts written by this user
+2) I think another was select all posts with category science or math
+
+3) the hardest question was like select all users with the more than 5 blogs post in scienc or math category
+But that was a bonus question they said you didn’t need to answer
+So you needed to do a group by and having
+
+Make sure to know left joins and null values too
