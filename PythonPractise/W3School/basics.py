8694f96ac76be479797f94b67dfa96b949753c47
Kirti Harode <kikihrd20@gmail.com>
2024-01-12 15:40:11 +0530

    type

--- PythonPractise/W3School/basics.py
+++ PythonPractise/W3School/basics.py
@@ -7,8 +7,8 @@ if 5 > 2:
     print("Five is greater than 2")
 
 # no declaration of variables, assign value with declaring
-x = 5
-y = 'Hello'
+x = 5   #int type
+y = 'Hello' # string type
 print(x)
 print(y)
 
@@ -18,3 +18,10 @@ written in
 more than just one line
 """
 
+a = int(3) #3
+b = float(3) #3.0
+c = str(3)  #'3'
+
+print(type(a))   #<class 'int'>
+print(type(b))
+print(type(c))
