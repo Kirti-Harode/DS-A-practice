073289d2d47816fc5130c2e04532d6b618c329dd
Kirti Harode <kikihrd20@gmail.com>
2024-01-13 15:35:45 +0530

    multiline comment

--- PythonPractise/W3School/basics.py
+++ PythonPractise/W3School/basics.py
@@ -6,9 +6,15 @@ print('hello world')
 if 5 > 2:
     print("Five is greater than 2")
 
-# no declaration of variables
+# no declaration of variables, assign value with declaring
 x = 5
 y = 'Hello'
 print(x)
 print(y)
+
+"""
+This is a comment
+written in
+more than just one line
+"""
 
