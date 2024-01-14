08062403566cda88429590a23260a28ec5bad42f
Kirti Harode <kikihrd20@gmail.com>
2024-01-14 15:32:39 +0530

    comments in python

--- PythonPractise/W3School/basics.py
+++ PythonPractise/W3School/basics.py
@@ -1,11 +1,14 @@
 # web development server side
+# print statement
 print('hello world')
 
-
+#conditional and indentation
 if 5 > 2:
     print("Five is greater than 2")
 
+# no declaration of variables
 x = 5
 y = 'Hello'
 print(x)
 print(y)
+
